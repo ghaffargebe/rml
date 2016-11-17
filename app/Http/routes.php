568897c85e351707e4a958c93c9453568764cb3c
ghaffarrizqi <ghaffargebe@gmail.com>
2016-11-17 16:15:46 +0700

    update route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,53 +10,50 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::get('rml/', 'HomeController@index');
-Route::group(['domain' => 'localhost:9000'], function () {
+Route::group(['prefix' => 'rml'], function(){
+        Route::get('users', function () {
+            echo "string";
+        });
 
+    Route::get('/', 'HomeController@index');
     Route::get('tes', function () {
         // return phpinfo() ;
         $a = Hash::make('bismillah');
         echo $a;
     });
-});
 
-Route::group(['prefix' => 'admin'], function(){
-    Route::get('users', function () {
-        echo "string";
+    Route::get('profil/{organisasi}','HomeController@profil');
+    Route::get('frontdataset','HomeController@frontdataset');
+    Route::get('getFrontDataset/{org}','HomeController@getFrontDataset');
+    Route::get('frontdatadetail/{id}','HomeController@frontdatadetail');
+    Route::get('download/{file}','HomeController@download');
+    Route::get('news','HomeController@news');
+    Route::get('frontberitadetail/{id}','HomeController@frontberitadetail');
+
+    Route::auth();
+
+    Route::group(['middleware' => ['web', 'auth']], function () {
+        Route::get('/dashboard','DashboardController@frontpage');
+
+        Route::resource('user','userController');
+        Route::get('getUser','userController@getUser');
+        Route::get('userProfile','userController@userProfile');
+
+        Route::get('dataset','datasetController@index');
+        Route::get('getDataset','datasetController@getDataset');
+        Route::get('dataset/create','datasetController@create');
+        Route::post('dataset','datasetController@store');
+        Route::get('dataset/{id}/edit','datasetController@edit');
+        Route::patch('dataset/{id}','datasetController@update');
+        Route::delete('dataset/{id}','datasetController@destroy');
+
+        Route::resource('organisasi', 'organisasiController');
+        Route::get('getOrganisasi', 'organisasiController@getOrganisasi');
+
+        Route::resource('berita', 'beritaController');
+        Route::get('getBerita', 'beritaController@getBerita');
+        Route::get('berita/{id}/publish', 'beritaController@publish');
+        Route::get('berita/{id}/unpublish', 'beritaController@unpublish');
+        Route::get('berita/{id}/headline', 'beritaController@headline');
     });
-});
-
-Route::get('rml/profil/{organisasi}','HomeController@profil');
-Route::get('rml/frontdataset','HomeController@frontdataset');
-Route::get('rml/getFrontDataset/{org}','HomeController@getFrontDataset');
-Route::get('rml/frontdatadetail/{id}','HomeController@frontdatadetail');
-Route::get('rml/download/{file}','HomeController@download');
-Route::get('rml/news','HomeController@news');
-Route::get('rml/frontberitadetail/{id}','HomeController@frontberitadetail');
-
-Route::auth();
-
-Route::group(['rml/middleware' => ['web', 'auth']], function () {
-    Route::get('rml/dashboard','DashboardController@frontpage');
-
-    Route::resource('rml/user','userController');
-    Route::get('rml/getUser','userController@getUser');
-    Route::get('rml/userProfile','userController@userProfile');
-
-    Route::get('rml/dataset','datasetController@index');
-    Route::get('rml/getDataset','datasetController@getDataset');
-    Route::get('rml/dataset/create','datasetController@create');
-    Route::post('rml/dataset','datasetController@store');
-    Route::get('rml/dataset/{id}/edit','datasetController@edit');
-    Route::patch('rml/dataset/{id}','datasetController@update');
-    Route::delete('rml/dataset/{id}','datasetController@destroy');
-
-    Route::resource('rml/organisasi', 'organisasiController');
-    Route::get('rml/getOrganisasi', 'organisasiController@getOrganisasi');
-
-    Route::resource('rml/berita', 'beritaController');
-    Route::get('rml/getBerita', 'beritaController@getBerita');
-    Route::get('rml/berita/{id}/publish', 'beritaController@publish');
-    Route::get('rml/berita/{id}/unpublish', 'beritaController@unpublish');
-    Route::get('rml/berita/{id}/headline', 'beritaController@headline');
 });
