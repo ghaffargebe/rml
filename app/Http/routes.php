5e70ce83f374f3805e7cddce75627f7d32ab78f6
ghaffarrizqi <ghaffargebe@gmail.com>
2016-11-17 16:54:07 +0700

    update route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-Route::group(['prefix' => 'rml'], function(){
+// Route::group(['prefix' => 'rml'], function(){
         Route::get('users', function () {
             echo "string";
         });
@@ -56,4 +56,4 @@ Route::group(['prefix' => 'rml'], function(){
         Route::get('berita/{id}/unpublish', 'beritaController@unpublish');
         Route::get('berita/{id}/headline', 'beritaController@headline');
     });
-});
+// });
